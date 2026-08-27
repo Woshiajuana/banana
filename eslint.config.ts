a512ff4a8705223216f7d45313b29bc977048ba3
chenzhigang <979703986@qq.com>
2026-08-27 15:14:26 +0800

    chore: 更新

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,14 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
+import globals from 'globals'
+import tseslint from 'typescript-eslint';
+
+
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.node }
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
   },
   tseslint.configs.recommended,
-]);
+  prettier,
+])
